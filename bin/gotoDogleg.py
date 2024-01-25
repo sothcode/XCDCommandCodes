0331b2c25aef09efa083c15455573718a001887f
Ross Corliss <rcorliss@mit.edu>
2024-01-25 13:32:00 -0500

    added debug flags and other things

--- bin/gotoDogleg.py
+++ bin/gotoDogleg.py
@@ -1,6 +1,6 @@
-#!/opt/homebrew/bin/python3
-import quickAssign
-import quickReport
+#!/usr/bin/python3
+from quickAssign import writeXCD2
+from quickReport import reportXCD2
 import sys
 import time
 #import random #for testing
@@ -16,7 +16,7 @@ import time
 #        return True, 9
 #    return True, ret
 
-#wrapper for the two-argument reportXCD2
+#wrapper for the two-returns reportXCD2
 def readback(arg):
     check,ret = reportXCD2([arg])
     if check==False:
@@ -37,7 +37,7 @@ status_busy_value=9
 
 #tuning settings
 sleeptime=0.5 #in seconds
-
+debug=True
 
 #check args
 
@@ -54,29 +54,46 @@ destination=sys.argv[1]
 #}
 
 #check if controller is busy.  If so, exit with explanation
-status=readback(status_name)
+if debug:
+    print("goto:  Check status:")
+    status=readback(status_name)
 
 if status==status_busy_value:
     print("NOT EXECUTED. Controller is busy.")
     sys.exit()
+try:
+    input = float(sys.argv[1])
+except ValueError:
+    print("Error: Not a valid number")
+    sys.exit()
 
-
-    
+if debug:
+    print("goto:  Set destination:")    
 #set controller destination to the argument
-writeXCD2([command_argument_name, destination])
+writeXCD2([command_argument_name, input])
 #set the controller flag to move
+if debug:
+    print("goto:  Set command 'move':")    
 writeXCD2([command_word_name,goto_command_value])
 
 #monitor the controller position and report at intervals of sleeptime
+if debug:
+    print("goto:  Check position:");
 position=readback(position_feedback_name)
+if debug:
+    print("goto:  Check status:");
 status=readback(status_name)
 while status==status_busy_value:
     print("position:",readback(position_feedback_name)," status:",status)
+    if debug:
+        print ("goto: loop: check status:")
     status=readback(status_name)
     time.sleep(sleeptime)
 
 #loop until controller busy flag is cleared
 
 #report final position and success
+if debug:
+     print ("goto: finishing up.  check status and readback:")
 print("DONE. gotoDogleg complete.  status:",readback(status_name)," position:",readback(position_feedback_name));
 
