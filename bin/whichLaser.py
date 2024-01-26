7bf989f9352aaf0ad8c4e7387e5962964c63d48a
Ross Corliss <rcorliss@mit.edu>
2024-01-26 17:24:24 -0500

    added handy which laser

---
+++ bin/whichLaser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+import os
+from quickReport import readback
+from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
+from variableDictionaryXCD2 import varUniqueID as AXID
+
+
+def whichLaser()
+    currentID = readback(ADDR['ID'])
+    IDlookup = {v:k for k, v in AXID.items()}
+    print(IDlookup[currentID])
+
+
+if __name__ == "__main__":
+    debug=True
+    whichLaser()
