2d912882780b3d6a577c06ba4657e8f76b572104
Ross Corliss <rcorliss@mit.edu>
2024-01-26 11:22:31 -0500

    nearly circular reference.  sorry.

--- bin/quickAssign.py
+++ bin/quickAssign.py
@@ -4,6 +4,7 @@ import serial
 from variableDictionaryXCD2 import varDict
 from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
 from variableDictionaryXCD2 import varStatusValues as STAT
+from quickReport import readback
 import sys
 import struct
 
