684de1497b8e0c127c83278fe99d608b18765f88
Ross Corliss <rcorliss@mit.edu>
2024-01-31 16:55:07 -0500

    typo

--- bin/stopXMS.sh
+++ bin/stopXMS.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #stty -F /dev/ttyUSB0 115200
 for controller in `ls /dev/ttyUSB*`; do
-    echo starting $controller
+    echo stopping $controller
     echo -e '\xE4\xA5\x00\xD5\x06\x03\x01\x0F\0xDA' > $controller
 done
