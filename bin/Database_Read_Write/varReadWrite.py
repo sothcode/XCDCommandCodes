52e23ca0748f473db24ac976258fcb15015b9b0f
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-01-31 13:57:07 -0500

    Update varReadWrite.py

--- bin/Database_Read_Write/varReadWrite.py
+++ bin/Database_Read_Write/varReadWrite.py
@@ -20,67 +20,63 @@ logName = "log.txt"
 def writeVar(varName = None, varValue = None, new = '0'):
     
     if varName is None:                 # Checks if any arguments were given. If not, gives argument info
-        print(" \
-     No arguments given. writeVar will assign the given variable and values to the VariableDictionary. writeVar parameters are: \n \
-     1) varName - Mandatory, specifies the variable name. String in quotes. \n \
-     2) varValue - Mandatory, specifies the variable value. \n \
-     3) new - Optional. If given as 'new', the given variable name and value will be added to VariableDictionary \n \
-     ")
+        print("No arguments given. writeVar will assign the given variable and values to the VariableDictionary. writeVar parameters are: \n \
+                1) varName - Mandatory, specifies the variable name. String in quotes. \n \
+                2) varValue - Mandatory, specifies the variable value. \n \
+                3) new - Optional. If given as 'new', the given variable name and value will be added to VariableDictionary")
         return False
         
-    else:
-        
-        #print("Current Working Directory " , os.getcwd())               # Checks current working directory and moves to the correct one
-        
-        #Checks current working directory
-        if os.path.exists(filePath):
-            os.chdir(filePath)
-        else:
-            print("Can't change the Current Working Directory")   
-            return False
-            
-        #Reads in the file as a dictionary
-        varDict = {}
-        with open(fileName) as fOld:
-            for line in fOld:
-                (k, v) = line.split()
-                varDict[(k)] = v
+    #print("Current Working Directory " , os.getcwd())               # Checks current working directory and moves to the correct one
     
-        datetimeNow = str(datetime.datetime.now())
-        userName = str(getpass.getuser())
+    #Checks current working directory
+    if os.path.exists(filePath):
+        os.chdir(filePath)
+    else:
+        print("Can't change the Current Working Directory")   
+        return False
         
-        # if varName in "list":             # Checks if list arg was given
-        #     print("Variable List:")
-        #     #print('%s %s\n' % (varDict, varDict[]))
-        #     print(varDict)                          # Prints the list of variables with the list argument. Could be nicer.
-        if varName in varDict.keys():             # Checks if varName is a key
-            if varValue is None:                    # Checks that a variable value was given
-                print("No variable value given. To see a current list of variables and their values, use listVar()")
-                return False
-            else:
-                with open(logName, "a") as f:     # Logs the change to the log for a change
-                    f.write('%s %s %s %s \n' % (varName, varValue, datetimeNow, userName ))
-                varDict[varName] = varValue         # Assigns the given varValue to the key varName
-                if new in "new":
-                    print("Variable name already exists. Existing variable will be changed.")
-                print("Variable " +str(varName) + " changed to = " + str(varValue) + " at " + datetimeNow + " by " + userName)
+    #Reads in the file as a dictionary
+    varDict = {}
+    with open(fileName) as fOld:
+        for line in fOld:
+            (k, v) = line.split()
+            varDict[(k)] = v
+
+    datetimeNow = str(datetime.datetime.now())
+    userName = str(getpass.getuser())
+    
+    # if varName in "list":             # Checks if list arg was given
+    #     print("Variable List:")
+    #     #print('%s %s\n' % (varDict, varDict[]))
+    #     print(varDict)                          # Prints the list of variables with the list argument. Could be nicer.
+    if varName in varDict.keys():             # Checks if varName is a key
+        if varValue is None:                    # Checks that a variable value was given
+            print("No variable value given. To see a current list of variables and their values, use listVar()")
+            return False
+        else:
+            with open(logName, "a") as f:     # Logs the change to the log for a change
+                f.write('%s %s %s %s \n' % (varName, varValue, datetimeNow, userName ))
+            varDict[varName] = varValue         # Assigns the given varValue to the key varName
+            if new in "new":
+                print("Variable name already exists. Existing variable will be changed.")
+            print("Variable " +str(varName) + " changed to = " + str(varValue) + " at " + datetimeNow + " by " + userName)
+            
+    else:                                       # If varName not in deictionary, checks for "new" command"
+        if new in 'new':                        # If "new" command is there, appends varName and varValue to the dict
+            with open(logName, "a") as f:     # Logs the change to the log for an addition
+                f.write('%s %s %s %s \n' % (varName, varValue, datetimeNow, userName  ))
+            print("Added " + str(varName) + " = " + str(varValue) + " to dictionary at " + datetimeNow + " by " + userName)
+            varDict[varName] = varValue 
+        else:                                   # If "new" command is absent, returns error message
+            print("Variable name " + str(varName) +" not recongized. If you want to add a new variable to the list, add 'new' as a third argument. For a list of variables, use listVar()")
+            return False
+    #print(varDict)
+    
+    with open(fileName, 'w') as fileNew:            # Writes over the original with the new values
+        for key, value in varDict.items():  
+            fileNew.write('%s %s\n' % (key, value))
                 
-        else:                                       # If varName not in deictionary, checks for "new" command"
-            if new in 'new':                        # If "new" command is there, appends varName and varValue to the dict
-                with open(logName, "a") as f:     # Logs the change to the log for an addition
-                    f.write('%s %s %s %s \n' % (varName, varValue, datetimeNow, userName  ))
-                print("Added " + str(varName) + " = " + str(varValue) + " to dictionary at " + datetimeNow + " by " + userName)
-                varDict[varName] = varValue 
-            else:                                   # If "new" command is absent, returns error message
-                print("Variable name " + str(varName) +" not recongized. If you want to add a new variable to the list, add 'new' as a third argument. For a list of variables, use listVar()")
-                return False
-        #print(varDict)
-        
-        with open(fileName, 'w') as fileNew:            # Writes over the original with the new values
-            for key, value in varDict.items():  
-                fileNew.write('%s %s\n' % (key, value))
-                
-        return True
+    return True
             
 
 def readVar(varName = None):
@@ -90,31 +86,31 @@ def readVar(varName = None):
      1) varName - Mandatory, specifies the variable name. String. \n \
      ")
         return False
+
+    #print("Current Working Directory " , os.getcwd())               # Checks current working directory and moves to the correct one
+    
+    #Checks current working directory
+    if os.path.exists(filePath):
+        os.chdir(filePath)
     else:
-        #print("Current Working Directory " , os.getcwd())               # Checks current working directory and moves to the correct one
+        print("Can't change the Current Working Directory") 
+        return False
         
-        #Checks current working directory
-        if os.path.exists(filePath):
-            os.chdir(filePath)
-        else:
-            print("Can't change the Current Working Directory") 
-            return False
-            
-        #Reads in the file as a dictionary
-        varDict = {}
-        with open(fileName) as fOld:
-            for line in fOld:
-                (k, v) = line.split()
-                varDict[(k)] = v
-        #print(varDict)
-        #Checks if varName is a key
-        if varName in varDict.keys():
-            print(varDict.get(varName))
-            return True, float(varDict.get(varName))         # Returns varValue as a float
-            
-        else:
-            print("Variable name not found")
-            return(False)
+    #Reads in the file as a dictionary
+    varDict = {}
+    with open(fileName) as fOld:
+        for line in fOld:
+            (k, v) = line.split()
+            varDict[(k)] = v
+    #print(varDict)
+    #Checks if varName is a key
+    if varName in varDict.keys():
+        print(varDict.get(varName))
+        return True, float(varDict.get(varName))         # Returns varValue as a float
+        
+    else:
+        print("Variable name not found")
+        return(False)
         
         
 def listVar():
