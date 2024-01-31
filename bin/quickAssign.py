5647dfc6781ec5a16b80d9b7cf7375c30d1bd380
Ross Corliss <rcorliss@mit.edu>
2024-01-31 12:00:17 -0500

    added missing import

--- bin/quickAssign.py
+++ bin/quickAssign.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import time
-import serial
+from xcdSerial import sendline
 from variableDictionaryXCD2 import varDict
 from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
 from variableDictionaryXCD2 import varStatusValues as STAT
