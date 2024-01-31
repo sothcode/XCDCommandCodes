0306e1d9820beb4e3918706bb5f476c7594fe36a
Ross Corliss <rcorliss@mit.edu>
2024-01-31 16:52:51 -0500

    missed a piece

--- bin/startXMS.sh
+++ bin/startXMS.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #stty -F /dev/ttyUSB0 115200
-for controller in /dev/ttyUSB*; do
+for controller in `ls /dev/ttyUSB*`; do
     echo starting $controller
     echo -e '\xE4\xA5\x00\xD5\x06\x03\x01\x0E\0xDA' > $controller
 done
