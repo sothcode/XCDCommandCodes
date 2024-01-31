cadc0b675a0512ffe3b7e41469b5c4bd129f9d11
Ross Corliss <rcorliss@mit.edu>
2024-01-31 16:38:12 -0500

    second axis in debug_test

--- bin/debug_test_doglegs.sh
+++ bin/debug_test_doglegs.sh
@@ -6,8 +6,15 @@ for ser in /dev/ttyUSB*; do
     if [ -e "$ser" ]; then
         # Run the command
 	echo "$ser" > XCD_current_port
+	echo "AXIS 0:"
         ./quickAssign FPOS 0
 	./quickAssign XAXIS 0
+	./gotoDogleg 2.9
+	./gotoDogleg -2.9
+	./gotoDogleg 0
+	echo "AXIS 1:"
+       ./quickAssign FPOS 0
+	./quickAssign XAXIS 1
 	./gotoDogleg 2.9
 	./gotoDogleg -2.9
 	./gotoDogleg 0
