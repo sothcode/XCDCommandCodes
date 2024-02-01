689e9d1d19d4ad73e980ade2569b6b12d93eef76
Ross Corliss <rcorliss@mit.edu>
2024-02-01 10:00:52 -0500

    minor.  cant put return in python outside of function

--- bin/gotoDogleg.py
+++ bin/gotoDogleg.py
@@ -15,7 +15,6 @@ from changeAxisDogleg import changeAxis
 sleeptime=0.6 #in seconds
 debug=False
 
-
 #check args
 if len(sys.argv)==2:
     #keep current leg, assume arg is destination.
@@ -87,4 +86,4 @@ if status==STAT['READY']:
     print("SUCCESS. gotoDogleg complete.  status:",readback(ADDR['STATUS'])," position:{:.4g} (ax{:.1g})".format(readback(ADDR['FPOS']),readback(ADDR['XAXIS'])), "nTurns:",readback(ADDR['TURNS']));
 else:
     print("FAIL. gotoDogleg failed.  status:",readback(ADDR['STATUS'])," position:{:.4g} (ax{:.1g})".format(readback(ADDR['FPOS']),readback(ADDR['XAXIS'])), "nTurns:",readback(ADDR['TURNS']));
-return lastpos
+#return lastpos
