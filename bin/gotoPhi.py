43c20f64d1b148b17fbba5d8a617a26db8a0d395
Ross Corliss <rcorliss@mit.edu>
2024-02-03 14:04:34 -0500

    cleanup of gotoPhi

--- bin/gotoPhi.py
+++ bin/gotoPhi.py
@@ -129,77 +129,10 @@ if __name__ == "__main__":
         inputval=sys.argv[2]
     else:
         print("NOT EXECUTED. Wrong number of arguments.  Correct usage is:")
-        print("     ./gotoDogleg.py [position]")
-        print("  or ./gotoDogleg.py L#_DL#_A# [position]")
+        print("     ./gotoPhi.py [position]")
+        print("  or ./gotoPhi.py L#_PH [position]")
         sys.exit()
     #if wrong arguments, exit with explanation
 
     gotoPhi(inputval)
-
-
-
-
-    ##############################################################################################################################
-
-
-
-
-
-
-
-#check args
-if len(sys.argv)==2:
-    #keep current leg, assume arg is destination.
-    inputval=sys.argv[1]
-elif len(sys.argv)==3:
-    #assume first arg is leg, assume second arg is destination.
-    changeAxis(sys.argv[1])
-    inputval=sys.argv[2]
-else:
-    print("NOT EXECUTED. Wrong number of arguments.  Correct usage is:")
-    print("     ./gotoPhi.py [position]")
-    print("  or ./gotoPhi.py L#_PH [position]")
-    sys.exit()
-#if wrong arguments, exit with explanation
-
-#{later:
-#get current rotations
-#calculate destination nRotations
-#if that's tolerable
-#}
-
-#check if controller is busy.  If so, exit with explanation
-if debug:
-    print("goto:  Check status:")
-status=readback(ADDR['STATUS'])
-
-if status!=0:
-    print("NOT EXECUTED. Controller status is not 0.")
-    sys.exit()
-try:
-    destination = float(inputval)
-except ValueError:
-    print("Error: Not a valid number")
-    sys.exit()
-
-sendcommand(COMM['GOTO'],destination) # this sleeps until it sees the status change from new_command
-
-#monitor the controller position and report at intervals of sleeptime
-if debug:
-    print("goto:  Check position:");
-position=readback(ADDR['FPOS'])
-if debug:
-    print("goto:  Check status:");
-status=STAT['BUSY']
-while status==STAT['BUSY']:
-    status=readback(ADDR['STATUS'])
-    hardstop1=readback(ADDR['HARD_STOP1'])
-    hardstop2=readback(ADDR['HARD_STOP2'])
-    print("position:",readback(ADDR['FPOS'])," status:",status, "lb:",hardstop1, "hb:",hardstop2)
-    if debug:
-        print ("goto: loop: check status:")
-    status=readback(ADDR['STATUS'])
-    time.sleep(sleeptime)
-
-#loop until controller busy flag is cleared
 
