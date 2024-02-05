b1a9aefeb9047806383474011ec80b85d54264be
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-02-05 12:55:07 -0500

    Reformatted gotoThetaL, gotoThetaS and continued refactoring

--- bin/gotoThetaS.py
+++ bin/gotoThetaS.py
@@ -14,66 +14,88 @@ from changeAxisDogleg import changeAxis
 sleeptime=0.5 #in seconds
 debug=False
 
-
-#check args
-if len(sys.argv)==2:
-    #keep current leg, assume arg is destination.
-    inputval=sys.argv[1]
-elif len(sys.argv)==3:
-    #assume first arg is leg, assume second arg is destination.
-    changeAxis(sys.argv[1])
-    inputval=sys.argv[2]
-else:
-    print("NOT EXECUTED. Wrong number of arguments.  Correct usage is:")
-    print("     ./gotoThetaS.py [position]")
-    print("  or ./gotoThetaS.py L#_TH_S [position]")
-    sys.exit()
-#if wrong arguments, exit with explanation
-
 #{later:
 #get current rotations
 #calculate destination nRotations
 #if that's tolerable
 #}
 
-#check if controller is busy.  If so, exit with explanation
-if debug:
-    print("goto:  Check status:")
-status=readback(ADDR['STATUS'])
+def _reverseLookup(dict,val):
+    #set up the reverse dictionary
+    reverse_mapping={v: k for k, v in dict.items()}
+    try:
+        key=reverse_mapping[val]
+    except KeyError as e:
+        print(f"errorCode lookup failed.  KeyError: {e}")
+        sys.exit()
+    return key  
 
-if status!=0:
-    print("NOT EXECUTED. Controller status is not 0.")
-    sys.exit()
-try:
-    destination = float(inputval)
-except ValueError:
-    print("Error: Not a valid number")
-    sys.exit()
 
-sendcommand(COMM['GOTO'],destination) # this sleeps until it sees the status change from new_command
-
-#monitor the controller position and report at intervals of sleeptime
-if debug:
-    print("goto:  Check position:");
-position=readback(ADDR['FPOS'])
-if debug:
-    print("goto:  Check status:");
-status=STAT['BUSY']
-while status==STAT['BUSY']:
-    status=readback(ADDR['STATUS'])
-    print("position:",readback(ADDR['FPOS'])," status:",status)
+def gotoThetaS(whereToGo):
+    #check if controller is busy.  If so, exit with explanation
     if debug:
-        print ("goto: loop: check status:")
+        print("goto:  Check status:")
     status=readback(ADDR['STATUS'])
-    time.sleep(sleeptime)
 
-#loop until controller busy flag is cleared
+    if status!=0:
+        print("NOT EXECUTED. Controller status is not 0.")
+        sys.exit()
+    try:
+        destination = float(whereToGo)
+    except ValueError:
+        print("Error: Not a valid number")
+        sys.exit()
 
-#report final position and success
-if debug:
-     print ("goto: finishing up.  check status and readback:")
-if status==STAT['READY']:
-    print("SUCCESS. gotoThetaS complete.  status:",readback(ADDR['STATUS'])," position:{:.4g}".format(readback(ADDR['FPOS'])));
-else:
-    print("FAIL. gotoThetaS failed.  status:",readback(ADDR['STATUS'])," position:{:.4g}".format(readback(ADDR['FPOS'])));
+    sendcommand(COMM['GOTO'],destination) # this sleeps until it sees the status change from new_command
 
+    #monitor the controller position and report at intervals of sleeptime
+    if debug:
+        print("goto:  Check position:");
+    position=readback(ADDR['FPOS'])
+    if debug:
+        print("goto:  Check status:");
+    status=STAT['BUSY']
+    while status==STAT['BUSY']:
+        status=readback(ADDR['STATUS'])
+        print("position:",readback(ADDR['FPOS'])," status:",status)
+        if debug:
+            print ("goto: loop: check status:")
+        status=readback(ADDR['STATUS'])
+        time.sleep(sleeptime)
+
+    #loop until controller busy flag is cleared
+
+    #report final position and success
+    if debug:
+        print ("goto: finishing up.  check status and readback:")
+    
+    position=readback(ADDR['FPOS'])
+    status=readback(ADDR['STATUS'])
+    hb=readback(ADDR['HARD_STOP2'])
+    lb=readback(ADDR['HARD_STOP1'])
+
+    if status==STAT['READY']:
+        print("SUCCESS. gotoThetaS complete.  status: %s (%s) position:%1.5f lb:%1.5f hb:%1.5f"%(status,_reverseLookup(STAT,status),position,lb,hb))
+        return True, position
+    else:
+        print("FAIL. gotoThetaS failed.  status: %s (%s) position:%1.5f lb:%1.5f hb:%1.5f"%(status,_reverseLookup(STAT,status),position,lb,hb))
+        return False, position
+
+
+if __name__ == "__main__":
+    #check args
+    if len(sys.argv)==2:
+        #keep current leg, assume arg is destination.
+        inputval=sys.argv[1]
+    elif len(sys.argv)==3:
+        #assume first arg is leg, assume second arg is destination.
+        changeAxis(sys.argv[1])
+        inputval=sys.argv[2]
+    else:
+        print("NOT EXECUTED. Wrong number of arguments.  Correct usage is:")
+        print("     ./gotoThetaS.py [position]")
+        print("  or ./gotoThetaS.py L#_TH_S [position]")
+        sys.exit()
+    #if wrong arguments, exit with explanation
+    
+    gotoThetaS(inputval)
--- refactor/xcdMotor.py
+++ refactor/xcdMotor.py
@@ -252,120 +252,119 @@ class Motor:
     def getVar(variable):
 
 
-def reportXCD2( argv ):
-    if argv:
-        if len(argv) > 10:
-            print("Too many variables trying to be assigned.  Max 10 variables can be assigned at once.")
-            return False, 0
+# def reportXCD2( argv ):
+#     if argv:
+#         if len(argv) > 10:
+#             print("Too many variables trying to be assigned.  Max 10 variables can be assigned at once.")
+#             return False, 0
 
-        var_names = argv
-        if debug:
-            print(var_names)
+#         var_names = argv
+#         if debug:
+#             print(var_names)
         
-        #getAxis = reportXCD2noAxis(['XAXIS'])[0]
+#         #getAxis = reportXCD2noAxis(['XAXIS'])[0]
 
-        ax_int = getAxis()
-        #int(getAxis)
-        #print("reportXCD2 axis=",ax_int)
-        ax_byte = ax_int.to_bytes(1,byteorder='little',signed=True)
-        ax_comm = [int(ax_byte[0])]
+#         ax_int = getAxis()
+#         #int(getAxis)
+#         #print("reportXCD2 axis=",ax_int)
+#         ax_byte = ax_int.to_bytes(1,byteorder='little',signed=True)
+#         ax_comm = [int(ax_byte[0])]
         
-        command = [228, 165, 0, 213, 0, 0, 6, 132]
-        count = 0
-        for i in range(0, len(var_names)):
-            var = var_names[i]
+#         command = [228, 165, 0, 213, 0, 0, 6, 132]
+#         count = 0
+#         for i in range(0, len(var_names)):
+#             var = var_names[i]
 
 
-            if var in varDict.keys():
-                var_num = varDict[var]
-                u2 = var_num.to_bytes(2,byteorder='little',signed=False)
-                var_command = [int(u2[0]), int(u2[1])]
-                command += var_command
-                count += 2
-            else:
-                print("Variable name - ", var , " -  not recognized. Variable list given as:")
-                print(varDict.keys())
-                return False, 0
+#             if var in varDict.keys():
+#                 var_num = varDict[var]
+#                 u2 = var_num.to_bytes(2,byteorder='little',signed=False)
+#                 var_command = [int(u2[0]), int(u2[1])]
+#                 command += var_command
+#                 count += 2
+#             else:
+#                 print("Variable name - ", var , " -  not recognized. Variable list given as:")
+#                 print(varDict.keys())
+#                 return False, 0
             
-            command += ax_comm
-            count += 1
+#             command += ax_comm
+#             count += 1
 
-    else:
-        print("No arguments given. reportXCD2 parameters are: \n \
-               1) Variable- Mandatory, variable to report value of. For full list of variables, refer to variableDictionary. \n \
-               2-10) Variable - Optional, other variables to report. \
-               ")
-        return False, 0
+#     else:
+#         print("No arguments given. reportXCD2 parameters are: \n \
+#                1) Variable- Mandatory, variable to report value of. For full list of variables, refer to variableDictionary. \n \
+#                2-10) Variable - Optional, other variables to report. \
+#                ")
+#         return False, 0
 
-    # reassign packet length and block length bytes
-    command[4] = int(count+6)
-    command[5] = int(count+3)
-    # add stop byte
-    command += [218]
-    if debug:
-        print(command)
+#     # reassign packet length and block length bytes
+#     command[4] = int(count+6)
+#     command[5] = int(count+3)
+#     # add stop byte
+#     command += [218]
+#     if debug:
+#         print(command)
 
-    # the next portion of code is what establishes communication with the controller
-    # and sends the bytestring command by serial comm
-    # and returns a pair of [succeeded,readbackline]
-    success,ret=sendline(getCurrentPort(),command)
-    if success and debug:
-        print(ret)
-    return success, ret
+#     # the next portion of code is what establishes communication with the controller
+#     # and sends the bytestring command by serial comm
+#     # and returns a pair of [succeeded,readbackline]
+#     success,ret=sendline(getCurrentPort(),command)
+#     if success and debug:
+#         print(ret)
+#     return success, ret
 
 
 
-def reportXCD2noAxis( argv ):
-    return reportXCD2noAxisPort(getCurrentPort(),argv)
+# def reportXCD2noAxis( argv ):
+#     return reportXCD2noAxisPort(getCurrentPort(),argv)
 
-def reportXCD2noAxisPort(target_port, argv ):
-    if argv:
-        if len(argv) > 10:
-            print("Too many variables trying to be assigned.  Max 10 variables can be assigned at once.")
-            return False, 0
+# def reportXCD2noAxisPort(target_port, argv ):
+#     if argv:
+#         if len(argv) > 10:
+#             print("Too many variables trying to be assigned.  Max 10 variables can be assigned at once.")
+#             return False, 0
 
-        var_names = argv
-        if debug:
-            print(var_names)
+#         var_names = argv
+#         if debug:
+#             print(var_names)
 
-        command = [228, 165, 0, 213, 0, 0, 6, 4]
-        count = 0
-        for i in range(0, len(var_names)):
-            var = var_names[i]
+#         command = [228, 165, 0, 213, 0, 0, 6, 4]
+#         count = 0
+#         for i in range(0, len(var_names)):
+#             var = var_names[i]
 
-            if var in varDict.keys():
-                var_num = varDict[var]
-                u2 = var_num.to_bytes(2,byteorder='little',signed=False)
-                var_command = [int(u2[0]), int(u2[1])]
-                command += var_command
-                count += 2
-            else:
-                print("Variable name - " , var , " -  not recognized. Variable list given as:")
-                print(varDict.keys())
-                return False, 0
+#             if var in varDict.keys():
+#                 var_num = varDict[var]
+#                 u2 = var_num.to_bytes(2,byteorder='little',signed=False)
+#                 var_command = [int(u2[0]), int(u2[1])]
+#                 command += var_command
+#                 count += 2
+#             else:
+#                 print("Variable name - " , var , " -  not recognized. Variable list given as:")
+#                 print(varDict.keys())
+#                 return False, 0
 
-    else:
-        print("No arguments given. reportXCD2 parameters are: \n \
-               1) Variable- Mandatory, variable to report value of. For full list of variables, refer to variableDictionary. \n \
-               2-10) Variable - Optional, other variables to report. \
-               ")
-        return False, 0
+#     else:
+#         print("No arguments given. reportXCD2 parameters are: \n \
+#                1) Variable- Mandatory, variable to report value of. For full list of variables, refer to variableDictionary. \n \
+#                2-10) Variable - Optional, other variables to report. \
+#                ")
+#         return False, 0
 
-    # reassign packet length and block length bytes
-    command[4] = int(count+6)
-    command[5] = int(count+3)
-    # add stop byte
-    command += [218]
-    if debug:
-        print(command) 
+#     # reassign packet length and block length bytes
+#     command[4] = int(count+6)
+#     command[5] = int(count+3)
+#     # add stop byte
+#     command += [218]
+#     if debug:
+#         print(command) 
 
-    # the next portion of code is what establishes communication with the controller
-    # and sends the bytestring command by serial comm
-    success,ret=sendline(target_port,command)
-    if success and debug:
-        print(ret)
-    return success, ret
-
+#     # the next portion of code is what establishes communication with the controller
+#     # and sends the bytestring command by serial comm
+#     success,ret=sendline(target_port,command)
+#     if success and debug:
+#         print(ret)
+#     return success, ret
         return
     
 
