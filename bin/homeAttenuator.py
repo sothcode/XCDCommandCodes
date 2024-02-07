1999b96725df08ac08ebedad9f4c8ebcaa2b97df
Ross Corliss <rcorliss@mit.edu>
2024-02-07 11:57:52 -0500

    improved readback and db check for homeAtt

--- bin/homeAttenuator.py
+++ bin/homeAttenuator.py
@@ -81,7 +81,7 @@ status=readback(ADDR['STATUS'])
 lb=readback(ADDR['HARD_STOP1'])
 hb=readback(ADDR['HARD_STOP2'])
 position=readback(ADDR['FPOS'])
-home=position
+home=readback(ADDR['HOME'])
 turns=readback(ADDR['TURNS'])
 axis=readback(ADDR['XAXIS'])
 
@@ -89,27 +89,31 @@ if debug:
      print ("home: finishing up.  check status and readback:")
 
     
-lbRel=hardstop1-home
-hbRel=hardstop2-home
+lbRel=hardstop1-position
+hbRel=hardstop2-position
 span=hardstop2-hardstop1
+homeIsReal=False
+if (home>hardstop1 and home<hardstop2):
+    homeIsReal=True
+homeRel=home-lbRel
 
 #sanity check
 if(span<0.75):
-    print("FAIL.  Span is less than 0.75 (expect ~0.80).  Motor likely jammed.  span:%s. lbrel:%s.hbrel:%s-%s=%s."%(span,lbRel,hbRel))
+    print("FAIL.  Span is less than 0.75 (expect ~0.80).  Motor likely jammed.  span:%s. lbrel:%s hbrel:%s home:%s%s%s."%(span,lbRel,hbRel, homeRel,"(Real)"*homeIsReal,"(NotFound)"*(not homeIsReal)))
     print("aborting.  Db will not be updated.")
     sys.exit()
 if(span>1.0):
-    print("FAIL.  Span is greater than 1?!?  Encoder likely damaged.  span:%s. lbrel:%s.hbrel:%s-%s=%s."%(span,lbRel,hbRel))
+    print("FAIL.  Span is greater than 1?!?  Encoder likely damaged.  span:%s. lbrel:%s hbrel:%s home:%s%s%s."%(span,lbRel,hbRel, homeRel,"(Real)"*homeIsReal,"(NotFound)"*(not homeIsReal)))
     print("aborting.  Db will not be updated.")
     sys.exit()
 if(abs(lbRel)>matchTolerance):
-    print("FAIL.  home and lowbound are not the same?  Motor likely jammed.  span:%s. lb:%s home:%s."%(span,lb,home))
+    print("FAIL.  home and lowbound are not the same?  Motor likely jammed.  span:%s. lbrel:%s hbrel:%s home:%s%s%s."%(span,lbRel,hbRel, homeRel,"(Real)"*homeIsReal,"(NotFound)"*(not homeIsReal)))
     print("aborting.  Db will not be updated.")
     sys.exit()
 #compare to db values
 
 if referenceEgg!=None:
-    print("Comparing to Db.  Current status: %s (%s) position:%1.6f axis:%s turns:%s lb:%1.5f hb:%1.5f"%(status,_reverseLookup(STAT,status),position,axis, turns,lb,hb))
+    print("Comparing to Db.  Current status: %s (%s) position:%1.6f axis:%s turns:%s lb:%1.5f hb:%1.5f home:%1.5f"%(status,_reverseLookup(STAT,status),position,axis, turns,lb,hb,home))
     match=True
     present=[True]*3
     success,homeDb=kfDatabase.readVar(mainDb,"%s/home"%referenceEgg)
@@ -132,22 +136,19 @@ if referenceEgg!=None:
     hbRelDb=hbDb-homeDb
     varLb=lbRel-lbRelDb
     varHb=hbRel-hbRelDb
+    varHome=homeRel-homeDb
     varSpan=span-spanDb
     if (
         abs(varLb)>matchTolerance or
         abs(varHb)>matchTolerance or
+        abs(varHome)>matchTolerance or
         abs(varSpan)>matchTolerance
     ):
         match=False
     if not match:
         if  (present[0] and present[1] and present[2]):
-            if(span<0.85):
-                print("FAIL.  Span is less than 0.85 (expect ~0.95).  Motor likely jammed.\n\tspan:%s-%s=%s.\n\tlbrel:%s-%s=%s.\n\thbrel:%s-%s=%s."%(matchTolerance,span,spanDb,varSpan,lbRel,lbRelDb,varLb,hbRel,hbRel,varHb))
-                print("aborting.  Db will not be updated.")
-                sys.exit()
-            else:
-                print("FAIL.  Readback-db residuals are larger than tolerance %s:\n\tspan:%s-%s=%s.\n\tlbrel:%s-%s=%s.\n\thbrel:%s-%s=%s."%(matchTolerance,span,spanDb,varSpan,lbRel,lbRelDb,varLb,hbRel,hbRel,varHb))
-                print("   This does not match the expectations for %s.  If you are sure this really is %s, and want to update %s with new parameters, run the following commands:"%(referenceEgg,referenceEgg,mainDb))
+            print("FAIL.  Readback-db residuals are larger than tolerance %s:\n\tspan:%s-%s=%s.\n\tlbrel:%s-%s=%s.\n\thbrel:%s-%s=%s\n\thome:%s-%s=%s."%(matchTolerance,span,spanDb,varSpan,lbRel,lbRelDb,varLb,hbRel,hbRel,varHb,homeRel,homeDb,varHome))
+            print("   This does not match the expectations for %s.  If you are sure this really is %s, and want to update %s with new parameters, run the following commands:"%(referenceEgg,referenceEgg,mainDb))
         if  (not present[0] or not present[1] or not present[2]):
             print("FAIL.  Not all values are in the database for egg %s."%(referenceEgg))
             print("   This does not match the expectations for %s.  If you are sure this really is %s, and want to update %s with new parameters, run the following commands:"%(referenceEgg,referenceEgg,mainDb))
@@ -160,13 +161,15 @@ if referenceEgg!=None:
     writeXCD2([ADDR['FPOS'], 0])
     writeXCD2([ADDR['HARD_STOP1'], lbRel])
     writeXCD2([ADDR['HARD_STOP2'], hbRel])
+    writeXCD2([ADDR['HOME'],homeRel])
     
     lb=readback(ADDR['HARD_STOP1'])
     hb=readback(ADDR['HARD_STOP2'])
     position=readback(ADDR['FPOS'])   
+    home=readback(ADDR['FPOS'])   
     
 
 if status==STAT['READY']:
-    print("SUCCESS. homeAttenuator complete. status: %s (%s) position:%1.6f axis:%s turns:%s lb:%1.5f hb:%1.5f"%(status,_reverseLookup(STAT,status),position,axis, turns,lb,hb))
+    print("SUCCESS. homeAttenuator complete. status: %s (%s) position:%1.6f axis:%s turns:%s lb:%1.5f hb:%1.5f home:%1.5f"%(status,_reverseLookup(STAT,status),position,axis, turns,lb,hb, home))
 else:
-    print("FAIL. homeAttenuator failed. status: %s (%s) position:%1.6f axis:%s turns:%s lb:%1.5f hb:%1.5f"%(status,_reverseLookup(STAT,status),position,axis, turns,lb,hb))
+    print("FAIL. homeAttenuator failed. status: %s (%s) position:%1.6f axis:%s turns:%s lb:%1.5f hb:%1.5f home:%1.5f"%(status,_reverseLookup(STAT,status),position,axis, turns,lb,hb,home))
