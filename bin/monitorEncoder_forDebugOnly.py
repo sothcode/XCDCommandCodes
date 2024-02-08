680b254c0ca74bed03c864f47107928514f7c8c4
Ross Corliss <rcorliss@mit.edu>
2024-02-08 10:37:57 -0500

    minor

--- bin/monitorEncoder_forDebugOnly.py
+++ bin/monitorEncoder_forDebugOnly.py
@@ -7,7 +7,7 @@ from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
 print("Monitoring both encoders on channel set by XCD_current_port:")
 
 while True:
-    s0,enc0=reportXCD2specifyAxis(0,[ADDR['ENC']])
-    s1,enc1=reportXCD2specifyAxis(0,[ADDR['ENC']])
+    s0,enc0=reportXCD2specifyAxis(0,[ADDR['ENR']])
+    s1,enc1=reportXCD2specifyAxis(0,[ADDR['ENR']])
     print("Enc0:%1.6f%s\tEnc1:%1.6f%s"%(enc0,"(error)"*s0,enc1,(error)*s1))
     
