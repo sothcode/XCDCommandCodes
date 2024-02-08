1b9fef5a9baf6d579deac57b7e14fe132add0494
Ross Corliss <rcorliss@mit.edu>
2024-02-08 10:38:22 -0500

    typo

--- bin/monitorEncoder_forDebugOnly.py
+++ bin/monitorEncoder_forDebugOnly.py
@@ -9,5 +9,5 @@ print("Monitoring both encoders on channel set by XCD_current_port:")
 while True:
     s0,enc0=reportXCD2specifyAxis(0,[ADDR['ENR']])
     s1,enc1=reportXCD2specifyAxis(0,[ADDR['ENR']])
-    print("Enc0:%1.6f%s\tEnc1:%1.6f%s"%(enc0,"(error)"*s0,enc1,(error)*s1))
+    print("Enc0:%1.6f%s\tEnc1:%1.6f%s"%(enc0,"(error)"*s0,enc1,"(error)"*s1))
     
