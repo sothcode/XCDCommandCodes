4149a802880ec051c83089103ff7a3874287ada4
Ross Corliss <rcorliss@mit.edu>
2024-02-09 10:16:57 -0500

    assignPorts now can setup a reasonable dogleg or a reasonable egg setup by default

--- bin/assignPorts_forDebugOnly.py
+++ bin/assignPorts_forDebugOnly.py
@@ -28,6 +28,7 @@ def _reverseLookup(dict,val):
     return key
 
 def assignUARTsToPort(port, id0, id1):
+    print("assigning %s:  ax0:%s ax1:%s"%(port,id0,id1))
     port_before=getCurrentPort()
     with open(PORTFILE, 'w') as file:
         file.write(port)
@@ -41,12 +42,34 @@ if __name__ == "__main__":
 
     #check args
     #note that sys.argv has arg 1 as the command itself
-    if len(sys.argv) != 1:
-        print("NOT EXECUTED. Wrong number of arguments.  Correct usage is ./assignPorts[tab]")
-        sys.exit()
     #if wrong arguments, exit with explanation
-
-    asssignUARTsToPort("/dev/ttyUSB0", ID['DEBUG_DL0_A1']], ID['DEBUG_DL0_A0']])
-    asssignUARTsToPort("/dev/ttyUSB1", ID['DEBUG_DL1_A1']], ID['DEBUG_DL1_A0']])
+    if len(sys.argv) == 1: #overall default
+        print("Assigning to ttyUSB0 and USB1 to default dogleg assumption:"
+        asssignUARTsToPort("/dev/ttyUSB0", ID['DEBUG_DL0_A1']], ID['DEBUG_DL0_A0']])
+        asssignUARTsToPort("/dev/ttyUSB1", ID['DEBUG_DL1_A1']], ID['DEBUG_DL1_A0']])
+    elif len(sys.argv)==2: #allow two possibilities:  'doglegs' or 'egg'.  do the default assumption for those.
+        if sys.argv[2]='doglegs':
+            print("Assigning to ttyUSB0 and USB1 to default dogleg assumption:"
+            asssignUARTsToPort("/dev/ttyUSB0", ID['DEBUG_DL0_A1']], ID['DEBUG_DL0_A0']])
+            asssignUARTsToPort("/dev/ttyUSB1", ID['DEBUG_DL1_A1']], ID['DEBUG_DL1_A0']])
+        elif sys.argv[2]='egg':
+            print("Assigning to ttyUSB0 and USB1 to default egg assumption:"
+            asssignUARTsToPort("/dev/ttyUSB0", ID['DEBUG_TH_L']], ID['DEBUG_TH_S']])
+            asssignUARTsToPort("/dev/ttyUSB1", ID['DEBUG_AT']], ID['DEBUG_PH']])
+        else:
+            print("no default assumption for '%s'.  Args are either 'doglegs' of 'egg'")
+            sys.exiit()
+    elif len(sys.argv)==3:
+        port=sys.argv[1]
+        idstr={}
+        idnum={}
+        for i in range(0,1)
+            idstr[i]=sys.argv[i+2]
+            idnum[i]=ID[idstr[i]]
+        print("Assigning %s with UARTS %s(%s) and %s(%s)"%(port,idstr[0],idnum[0],idstr[1],idnum[1]))
+        assignUARTsToPort(port,idnum[0],idnum[1])
+    else:
+        print("NOT EXECUTED. Wrong number of arguments.  Correct usage is ./assignPorts[tab] [doglegs/egg] or ./assignPorts[tab] port,  name0, name1")        
+        sys.exit()
      
     print("Done.")
