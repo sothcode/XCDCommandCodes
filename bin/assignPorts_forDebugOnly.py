fec0331885f0a5b825c6f3aee6538babf8ffa227
Ross Corliss <rcorliss@mit.edu>
2024-02-16 15:45:00 -0500

    minor

--- bin/assignPorts_forDebugOnly.py
+++ bin/assignPorts_forDebugOnly.py
@@ -36,9 +36,11 @@ def assignUARTsToPort(port, id0, id1):
     writeXCD2(['UART0_ADDRESS', id0])
     writeXCD2(['UART1_ADDRESS', id1])    
     writeXCD2([ADDR['STATUS'], 0])    #clear the 'boot' status for this board now that we have assigned it.
+    print("assigned %s:  ax0:%s ax1:%s. status:%s==>%s"%(port,id0,id1,oldStatus,readback(ADDR['STATUS'])))
+
+    #return to what the active was before we began:
     with open(PORTFILE, 'w') as file:
         file.write(port_before)
-    print("assigned %s:  ax0:%s ax1:%s. status:%s==>%s"%(port,id0,id1,oldStatus,readback(ADDR['STATUS'])))
 
 
 if __name__ == "__main__":
