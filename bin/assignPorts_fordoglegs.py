0a97172f66327c178482c1e7871688edad388e66
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-04-24 04:32:03 -0400

    new assignPorts scripts
    assignPorts_forDebugOnly only allows DEBUG_## files to be updated and assignPorts_forLaser requires all 4 USBs to be plugged in.  The new scripts are temporary given the IO error with 4 USBs

---
+++ bin/assignPorts_fordoglegs.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+
+# a quick function to set the UARTS on ttyUSB0 and ttyUSB1 so that updatePorts sees two doglegs.
+from quickAssign import writeXCD2
+from quickReport import readback
+from xcdSerial import getCurrentPort
+import sys
+import time
+from variableDictionaryXCD2 import varUniqueID as ID
+from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
+from variableDictionaryXCD2 import varStatusValues as STAT
+
+
+#tuning settings
+sleeptime=0.5 #in seconds
+debug=False
+PORTFILE="XCD_current_port"
+
+
+def _reverseLookup(dict,val):
+    #set up the reverse dictionary
+    reverse_mapping={v: k for k, v in dict.items()}
+    try:
+        key=reverse_mapping[val]
+    except KeyError as e:
+        print("reverse lookup failed.  KeyError: %s"%(e))
+        sys.exit()
+    return key
+
+def assignUARTsToPort(port, id0, id1):
+    #print("assigning %s:  ax0:%s ax1:%s"%(port,id0,id1))
+    port_before=getCurrentPort()
+    with open(PORTFILE, 'w') as file:
+        file.write(port)
+    oldStatus=readback(ADDR['STATUS'])
+    writeXCD2(['UART0_ADDRESS', id0])
+    writeXCD2(['UART1_ADDRESS', id1])    
+    writeXCD2([ADDR['STATUS'], 0])    #clear the 'boot' status for this board now that we have assigned it.
+    writeXCD2([ADDR['XAXIS'], 0]) #always default to axis zero when we're assigning
+    #writeXCD2([ADDR['ID'], id0) #and write the proper ID to it.  except this will provoke the changeAxis to overwrite possibly valid data in the file.
+    print("assigned %s:  ax0:%s ax1:%s. status:%s==>%s"%(port,id0,id1,oldStatus,readback(ADDR['STATUS'])))
+
+    #return to what the active was before we began:
+    with open(PORTFILE, 'w') as file:
+        file.write(port_before)
+
+def assignPorts_forDogleg(laserPosition):
+    #check if laser is in the allowed list:
+    valid_names={"3S","6S","9S","12S","3N","6N","9N","12N","DEBUG"}
+    if not (laserPosition in valid_names):
+        print("Cannot assign ports for laser %s.  Laser must be in the list %s"%(laserPosition,valid_names))
+        return False
+
+    #now we are guaranteed we have a valid laser argument
+    assignUARTsToPort("/dev/ttyUSB0", ID["%s_DL0_A0"%(laserPosition)], ID["%s_DL1_A0"%(laserPosition)])
+    assignUARTsToPort("/dev/ttyUSB1", ID["%s_DL0_A1"%(laserPosition)], ID["%s_DL1_A1"%(laserPosition)])
+
+
+if __name__ == "__main__":
+
+    #check args
+    #note that sys.argv has arg 1 as the command itself
+    #if wrong arguments, exit with explanation
+    if len(sys.argv) == 2: #overall default
+        print("Trying to assign ttyUSB0 and USB1 to standard laser dogleg configuration:")
+        assignPorts_forDogleg(sys.argv[1])
+        print("Note:  This has NOT assigned the ID of the current axis (axis 0), so your first changeAxis will write to junk file and read in from the correct.")
+        print("Be sure to changeAxis before you begin to steer.")
+    else:
+        print("NOT EXECUTED. Wrong number of arguments.  Correct usage is ./assignPorts_forEgg.py 12S (etc)")
+        sys.exit()
+     
+    print("Done.")
