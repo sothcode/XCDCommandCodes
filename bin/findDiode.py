5dce39621ce2d41869fcd0d942760fcc96cfa670
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-07-02 18:09:37 -0400

    Update findDiode.py

--- bin/findDiode.py
+++ bin/findDiode.py
@@ -379,7 +379,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         axis = sys.argv[1]
         spiralSearch(axis)
-    elif len(sys.argv) == 10:
+    elif len(sys.argv) == 11:
         axis, x_DL00, x_DL01, x_DL10, x_DL11, ns0, ns1, step_size, autorun, img_bool = sys.argv[1:10]
         spiralSearch(axis, x_DL00, x_DL01, x_DL10, x_DL11, ns0, ns1, step_size, autorun, img_bool)
     else:
